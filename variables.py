709e15ca1551c22a32b80d37b234a38a29e193a8
rushikeshtrikal17 <rushikeshtrikal53@gmail.com>
2026-02-28 23:40:57 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,237 @@
+# #Ek variable a me value 10 store karo aur print karo.
+# a=10
+# print(a)
+# #Do variables x = 5 aur y = 7 ka sum print karo.
+# x=5
+# y=7
+# sum=x+y
+
+# print(sum)
+# #Ek variable name me apna naam store karke print karo.
+
+# #age = 21 store karke print karo:
+
+# name="rushikesh anil trikal "
+# age=21
+# print("my name is:",name,  "\nmy age is:",age)
+# #Ek variable ka type print karo (int, float, ya str).
+# a=1
+# b="c"
+# v=3.3
+# print(type(a))
+# print(type(b))
+# print(type(v))
+
+# #Teen variables a, b, c me 10, 20, 30 store karo aur unka total print karo.
+# a=10
+# b=20
+# c=30
+
+# total=a+b+c
+# print(total)
+
+# #length = 5 aur width = 3 se rectangle ka area nikaalo.
+
+# length = 5
+# width = 3
+
+# area=length*width
+
+# print("area of rectange is:", area)
+# #p = 100, r = 5, t = 2 se simple interest calculate karo.
+# p = 100
+# r = 5
+# t = 2
+# si=p*r*t/100
+
+# print("simple itrest is =", si)
+
+
+# #Do variables ki values swap karo (without using third variable).
+
+
+# a=3
+# b=4
+# print(a,b)
+# a,b=b,a
+# print(a,b)
+
+# #Ek hi line me 3 variables assign karo aur print karo.
+# m,w,d=11,2,22
+# print(m,w,d)
+
+
+# #User se ek number input lo aur use print karo
+
+# num=int(input("Enter The Number : "))
+# print(num)
+
+# #User se 2 numbers input leke unka sum print karo.
+
+# num1=int(input("enter the first number :"))
+# num2=int(input("enter the second number :"))
+# sum=num1+num2
+# print("the sum of your number is :", sum)
+
+# #User se naam input lo aur print karo:
+# #"Hello Rushi"
+
+# name=input("enter the name :")
+# print("Hello",name)
+
+# #User se ek number lo aur uska square print karo.
+
+# num3=int(input("enter number:"))
+# sq=num3*num3
+# print("the square of your given number is :",sq)
+
+# #User se age input lo aur batao wo adult hai ya nahi (age ≥ 18).
+
+# age=int(input("enter your age :"))
+
+# if age>=18:
+#     print("now you are adult ")
+
+
+# else:
+#     print("you are not adult go home ")
+
+
+# #Celsius ko Fahrenheit me convert karo.    
+
+# Celsius=float(input(f"emter how many celsius to convert to fahrenheit:"))
+# Fahrenheit= Celsius*9/5+32
+
+# print(f"{Celsius}°C \n in farenheit is \n {Fahrenheit}°F")
+
+# #Do numbers me se bada number print karo.
+
+# a=2
+# b=4
+# if a>b:
+#     print(a)
+
+# else:
+#     print(b)   
+
+# aa=22
+# bb=44
+# print(max(aa,bb))    
+
+
+# #Ek number even hai ya odd, check karo.
+
+# #a = 10, b = 20
+# a = 10
+# b = 20
+
+# if a%2==0:
+#     print(f"{a} is even")
+# else:
+#     print(f"{a}is odd")
+    
+# if b%2==0:
+#     print(f"{b} is even")
+
+# else:
+#     print(f"{b} is odd")
+
+
+# #User se name, age, city input lo aur ek hi line me print karo:
+# naam=input("enter name :")
+# age=int(input("enter age:"))
+# city=input("enter city")
+# print(f"my name is {naam} and my age is {age} and i live in {city}")
+
+
+# #Ek variable ko delete karo aur print karne ki koshish karo (error samjho).
+# r=12
+# del r
+# print(r)
+
+
+
+name="rushi"
+age=27
+price=2700
+price=2000
+print(type(price))
+print(price+price)
+a=222
+b=2.2
+surname="trikal"
+print(name+surname)
+print(b)
+del b
+#print(b)
+print(type(price))
+v=3
+c=3
+d=3
+
+
+length=40
+weigth=20
+area=length*weigth
+print(area)
+
+import math
+radius=10
+area=math.pi*radius*radius
+
+print(area)
+
+
+celsius=32
+fahrenheit=(32+9//5)+32
+print("fahrenheit=",fahrenheit)
+
+p=1000
+r=0.04
+time=28
+intrest=(p*r*time)/100
+print("intrest=",intrest)
+
+
+marks=400
+outoff=600
+per=400/600*100
+print("per=",per)
+
+
+km=55
+mitre=km*1000
+print("mitre=",mitre)
+
+
+r=100
+paisa=r*100
+print(r, "in rupees is =",paisa)
+
+h=4.40
+mint=h*59
+print(f"{h} in minute is ={mint}")
+
+a = 10
+b = 20
+
+temp = a
+a = b
+b = temp
+
+print("a =", a)
+print("b =", b)
+
+
+
+
+a = 10
+b = 20
+
+a, b = b, a
+
+print("a =", a)
+print("b =", b)
+
+
+
